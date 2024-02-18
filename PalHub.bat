a1c66b666ad6930cd713e927c92cabd430c20212
Bobjobob24 <bobsgaming24@gmail.com>
2024-02-17 20:05:01 -0600

    v0.0.2
    Now with backups!

--- PalHub.bat
+++ PalHub.bat
@@ -10,8 +10,20 @@ rem ============================================================================
 
 rem === Settings ===
 
+rem 7Zip Location
+set ZIP_DIR="C:\Program Files\7-Zip\7z.exe"
+
 rem Server Location
 set SERVER_DIR="C:\PalworldServer"
+
+rem Save Backup Location
+set SAVEBACKUP_DIR="C:\PalworldServerBackups\Saves"
+
+rem Config Backup Location
+set CONFIGBACKUP_DIR="C:\PalworldServerBackups\Config"
+
+rem Purge Interval in Minutes (1440 = 24 Hours)
+set PURGE_INTERVAL="1440"
 
 rem Restart Interval in Seconds (10800 = 3 Hours)
 set RESTART_INTERVAL="10800"
@@ -21,9 +33,11 @@ rem === End Settings ===
 rem ==============================================================================================================================================
 
 rem === Vars ===
+set SAVE_DIR="%SERVER_DIR%\Pal\Saved\SaveGames\0"
+set CONFIG_DIR="%SERVER_DIR%\Pal\Saved\Config\WindowsServer\PalWorldSettings.ini"
 set BIN_DIR="%SERVER_DIR%\Pal\Binaries\Win64"
 set EXE="PalServer-Win64-Test-Cmd.exe"
-set VER=v0.0.1
+set VER=v0.0.2
 rem === End Vars ===
 
 
@@ -31,7 +45,7 @@ rem === End Vars ===
 rem === Main Content ===
 :main
 cls
-call :title
+call :titlefnc
 
 rem == Start Server ==
 :start
@@ -40,7 +54,7 @@ echo:
 echo:
 timeout /t 3 /nobreak > nul
 
-call :startserver
+call :startserverfnc
 
 call :colorEcho 0a "[PalHub] Server Started"
 echo:
@@ -64,15 +78,30 @@ call :colorEcho 04 "[PalHub] Stopping Server"
 echo:
 echo:
 
-call :stopserver
+call :stopserverfnc
 timeout /t 2 /nobreak > nul
 
 call :colorEcho 0c "[PalHub] Server Stopped"
 echo:
 echo:
 timeout /t 5 /nobreak > nul
+goto backup
 rem == End Stop Server ==
+
+rem == Backup ==
+:backup
+call :backupfnc
+timeout /t 5 /nobreak > nul
+goto purge
+rem == End Backup ==
+
+rem == Purge ==
+:purge
+call :purgefnc
+timeout /t 5 /nobreak > nul
 goto main
+rem == End Purge ==
+
 
 rem === End Main Content ===
 
@@ -80,20 +109,8 @@ rem ============================================================================
 
 rem === Functions ===
 
-rem == Start Server FNC ==
-:startserver
-start /min /d "%BIN_DIR%" PalServer-Win64-Test-Cmd.exe
-goto :eof
-rem == End Start Server FNC ==
-
-rem == Stop Server FNC ==
-:stopserver
-taskkill /f /im %EXE% > nul
-goto :eof
-rem == End Stop Server FNC ==
-
 rem == Title FNC ==
-:title
+:titlefnc
 title PalHub %VER% by AriesLR
 call :colorEcho 0b "[Title] PalHub %VER% by AriesLR"
 echo:
@@ -101,6 +118,82 @@ echo:
 echo:
 goto :eof
 rem == End Title FNC ==
+
+rem == Time & Date FNC ==
+:datefnc
+rem Set variables for date & time output.
+set CUR_YYYY=%date:~10,4%
+set CUR_MM=%date:~4,2%
+set CUR_DD=%date:~7,2%
+set CUR_HH=%time:~0,2%
+if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
+set CUR_NN=%time:~3,2%
+set DATE=%CUR_MM%-%CUR_DD%-%CUR_YYYY%_%CUR_HH%-%CUR_NN%
+goto :eof
+rem == End Time & Date FNC ==
+
+rem == Start Server FNC ==
+:startserverfnc
+start /min /d "%BIN_DIR%" PalServer-Win64-Test-Cmd.exe
+goto :eof
+rem == End Start Server FNC ==
+
+rem == Stop Server FNC ==
+:stopserverfnc
+taskkill /f /im %EXE% > nul
+goto :eof
+rem == End Stop Server FNC ==
+
+rem == Backup FNC ==
+:backupfnc
+call :datefnc
+
+call :colorEcho 05 "[PalHub] Backup Save Files"
+echo:
+echo:
+timeout /t 1 /nobreak > nul
+
+%ZIP_DIR% a -tzip "%SAVEBACKUP_DIR%\SaveBackup_%DATE%.zip" "%SAVE_DIR%" > nul
+timeout /t 6 /nobreak > nul
+
+call :colorEcho 05 "[PalHub] Backup Config Files"
+echo:
+echo:
+timeout /t 1 /nobreak > nul
+
+%ZIP_DIR% a -tzip "%CONFIGBACKUP_DIR%\ConfigBackup_%DATE%.zip" "%CONFIG_DIR%" > nul
+timeout /t 6 /nobreak > nul
+
+call :colorEcho 0d "[PalHub] All Backups Complete"
+echo:
+echo:
+timeout /t 1 /nobreak > nul
+
+goto :eof
+rem == End Backup FNC ==
+
+rem == Purge FNC ==
+:purgefnc
+call :colorEcho 01 "[PalHub] Purging Save Backups"
+echo:
+echo:
+timeout /t 2 /nobreak > nul
+powershell.exe -ExecutionPolicy Bypass -Command "Get-ChildItem -path %SAVEBACKUP_DIR% | where {$_.Lastwritetime -lt (date).addminutes(-%PURGE_INTERVAL%)} | remove-item"
+
+call :colorEcho 01 "[PalHub] Purging Config Backups"
+echo:
+echo:
+timeout /t 2 /nobreak > nul
+powershell.exe -ExecutionPolicy Bypass -Command "Get-ChildItem -path %CONFIGBACKUP_DIR% | where {$_.Lastwritetime -lt (date).addminutes(-%PURGE_INTERVAL%)} | remove-item"
+
+call :colorEcho 09 "[PalHub] Purge Complete"
+echo:
+echo:
+timeout /t 2 /nobreak > nul
+goto :eof
+rem == End Purge FNC ==
+
+
 
 rem === End Functions ===
 
--- README.md
+++ README.md
@@ -6,35 +6,35 @@ PalHub is a lightweight server manager specifically designed for Palworld server
 
 - **Server Start**: Start your Palworld server with a single click, getting it up and running quickly.
 - **Automated Restart**: Set up automated restarts for your server at customizable intervals to keep it fresh and optimized.
+- **Backup and Purge**: Automatically backup your save and config files when the server restarts, and purge old backups to save disk space.
 - **Simple Setup**: Easy configuration through the script, requiring minimal effort to get started.
 
 ## Requirements
 
 - Palworld Server installed on your system.
 - Windows operating system.
+- [7-Zip](https://www.7-zip.org/) installed to perform backups.
 
 ## Usage
 
 1. **Configure PalHub:**
-   - Edit the script (`PalHub.bat`) to set your server directory (`SERVER_DIR`) and restart interval (`RESTART_INTERVAL`) according to your setup.
+   - Edit the script (`PalHub.bat`) to set your server directory (`SERVER_DIR`), backup directories (`SAVEBACKUP_DIR` and `CONFIGBACKUP_DIR`), purge interval (`PURGE_INTERVAL`), and restart interval (`RESTART_INTERVAL`) according to your setup.
 
 2. **Start Server:**
    - Run the script `PalHub.bat`.
    - PalHub will start your Palworld server.
 
-3. **Automate Restart (Optional):**
-   - PalHub automatically waits for the specified interval (`RESTART_INTERVAL`) and restarts the server.
-   - Customize the interval by adjusting the `RESTART_INTERVAL` variable in the script.
-
 ## Script Explanation
 
 - **`SERVER_DIR`**: Set the directory where your Palworld server is located.
+- **`SAVEBACKUP_DIR`**: Set the directory to store backup files for saves.
+- **`CONFIGBACKUP_DIR`**: Set the directory to store backup files for config.
+- **`PURGE_INTERVAL`**: Set the interval (in minutes) to purge old backups.
 - **`RESTART_INTERVAL`**: Set the interval (in seconds) for automated server restarts.
 
 ## Customization
 
 - You can modify the script to add more functionalities or tailor it to your specific requirements.
-- For advanced customization or additional features, consider extending the script or integrating it with other tools.
 
 ## Contributing
 
@@ -43,3 +43,8 @@ Contributions are welcome! If you have any suggestions, improvements, or bug fix
 ## License
 
 This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+- [Palworld](https://www.pocketpair.jp/palworld) - For inspiring this project.
+- [7-Zip](https://www.7-zip.org/) - For providing the tools necessary for backup functionality.
